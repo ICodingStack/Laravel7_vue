d9621d9875e4029435fb7d99d1d8335994cce70c
IBRA <ibrahimmerhi@mail.ru>
2020-12-31 20:30:24 +0800

    add models and make relationships

---
+++ app/Question.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $table="questions";
+    protected $primaryKey="id";
+    protected $fillable =[
+        'id',
+        'title',
+        'slug',
+        'body',
+        'views',
+        'answers',
+        'votes',
+        'best_answer',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
